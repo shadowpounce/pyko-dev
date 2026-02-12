bd440f459833c4e1ff6524831c6381a41354c8ee
Roman Kaniuka <roman.kaniuka.dev@gmail.com>
2026-02-12 19:37:47 +0200

    values animations

--- src/app/about/sections/Values/Values.tsx
+++ src/app/about/sections/Values/Values.tsx
@@ -4,6 +4,10 @@ import s from './Values.module.css'
 import Marquee from 'react-fast-marquee'
 import { useElementAnimationDelay, useSectionAnimationTrigger } from '@/hooks/useSectionAnimationTrigger'
 import Dots from '@/assets/icons/dots.svg'
+import { useGSAP } from '@gsap/react'
+import { useRef } from 'react'
+import gsap from 'gsap'
+import { SplitText } from 'gsap/SplitText'
 
 const cards = [
 	{
@@ -28,27 +32,57 @@ const cards = [
 	},
 ]
 export const Values = ({ sectionIndex }: { sectionIndex: number }) => {
-	const SECTION_INDEX = sectionIndex
-
-	const START_DELAY = 0.8
-
+	const containerRef = useRef<HTMLDivElement>(null)
+	const marqueeRef = useRef<HTMLDivElement>(null)
+	const videoContainerRef = useRef<HTMLDivElement>(null)
+	const titleRef = useRef<HTMLParagraphElement>(null)
 	const baseDelay = useSectionAnimationTrigger({
-		sectionIndex: SECTION_INDEX,
-		startDelay: START_DELAY,
+		sectionIndex: sectionIndex,
+		startDelay: 0.8,
 	})
-	const labelDelay = useElementAnimationDelay(baseDelay, 0)
-	const titleDelay = useElementAnimationDelay(baseDelay, 1)
+	const isInView = !!baseDelay
+	const labelDelay = useElementAnimationDelay(baseDelay, 3)
+
+	useGSAP(() => {
+		if (isInView) {
+			const split = SplitText.create(titleRef.current, { type: 'words' })
+			const cards = gsap.utils.toArray(`.${s.card}`)
+			const cardsTitle = gsap.utils.toArray<HTMLParagraphElement>(`.${s.cardTitle}`)
+			console.log('🚀 ~ Values ~ cardsTitle:', cardsTitle)
+			const cardsDescription = gsap.utils.toArray(`.${s.cardDescription}`)
+			const tl = gsap.timeline({
+				defaults: { duration: 0.6, ease: 'power2.out' }, delay: baseDelay || 1
+			})
+			tl.from(marqueeRef.current, { y: -300, autoAlpha: 0, filter: 'blur(5px)' })
+			tl.from(videoContainerRef.current, { scale: 0.8, y: 190, autoAlpha: 0, filter: 'blur(5px)', ease: 'back.out' }, '<+=0.2')
+			tl.from(split.words, { y: 30, autoAlpha: 0, filter: 'blur(5px)', ease: 'back.out', stagger: 0.1 })
+			tl.from(cards, {
+				y: 30, scale: 0.8, autoAlpha: 0, filter: 'blur(5px)', ease: 'back.out', stagger: {
+					each: 0.1,
+					grid: [2, 2],
+				}
+			}, '<+=0.2')
+
+			tl.from(cardsTitle, {
+				y: 30, autoAlpha: 0, filter: 'blur(5px)', stagger: 0.1
+			}, '<+=0.1')
+
+			tl.from(cardsDescription, {
+				y: 30, autoAlpha: 0, filter: 'blur(5px)', stagger: 0.1
+			}, '<+=0.1')
+		}
+	}, { dependencies: [isInView, baseDelay], scope: containerRef });
 	return (
 		<Section>
-			<div className={s.container}>
-				<Marquee autoFill speed={150} className={s.marquee}><p className={s.marqueeText}>Pyko evolves — quietly, constantly, with you.</p></Marquee>
+			<div ref={containerRef} className={s.container}>
+				<Marquee ref={marqueeRef} autoFill speed={150} className={s.marquee}><p className={s.marqueeText}>Pyko evolves — quietly, constantly, with you.</p></Marquee>
 				<div className={s.contentContainer}>
-					<div className={s.videoWrapper}>
+					<div ref={videoContainerRef} className={s.videoWrapper}>
 						<video src='videos/about/sec-4/pyko2-sec4-720p_j3MclSyk.mp4' muted autoPlay loop controls={false}></video>
 					</div>
 					<div className={s.content}>
 						<SectionLabel animationDelay={labelDelay} className={s.label}>Values</SectionLabel>
-						<p className={s.title}>What guides us</p>
+						<p ref={titleRef} className={s.title}>What guides us</p>
 						<div className={s.cardsContainer}>
 							{cards.map(card => <div key={card.id} className={s.card}>
 								<Dots />
