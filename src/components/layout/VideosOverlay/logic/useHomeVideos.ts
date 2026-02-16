cc4d638bb1d1ad3fa2c2adeecb472efb9a41646d
deep <minxkun@gmail.com>
2026-02-16 21:54:36 +0200

    adaptive updates for home page

--- src/components/layout/VideosOverlay/logic/useHomeVideos.ts
+++ src/components/layout/VideosOverlay/logic/useHomeVideos.ts
@@ -361,33 +361,33 @@ export const useHomeVideos = (
 
     return {
         videos: [
-            // {
-            //     id: 0,
-            //     src: 'videos/sec1-2/sec1-2-720p.mp4',
-            // },
-            // {
-            //     id: 1,
-            //     src: 'videos/sec3/sec3-720p.mp4',
-            // },
-            // {
-            //     id: 2,
-            //     src: 'videos/sec6/sec6-480p.mp4',
-            // },
-            // {
-            //     id: 3,
-            //     src: 'videos/sec7/sec7-480p.mp4',
-            // },
-            // {
-            //     id: 4,
-            //     src: 'videos/sec8-9-10/sec8-9-10-720p.mp4',
-            // },
+            {
+                id: 0,
+                src: 'videos/sec1-2/sec1-2-720p.mp4',
+            },
+            {
+                id: 1,
+                src: 'videos/sec3/sec3-720p.mp4',
+            },
+            {
+                id: 2,
+                src: 'videos/sec6/sec6-480p.mp4',
+            },
+            {
+                id: 3,
+                src: 'videos/sec7/sec7-480p.mp4',
+            },
+            {
+                id: 4,
+                src: 'videos/sec8-9-10/sec8-9-10-720p.mp4',
+            },
         ] as VideoConfig[],
         videoSources: [
-            // 'videos/sec1-2/sec1-2-720p.mp4',
-            // 'videos/sec3/sec3-720p.mp4',
-            // 'videos/sec6/sec6-480p.mp4',
-            // 'videos/sec7/sec7-480p.mp4',
-            // 'videos/sec8-9-10/sec8-9-10-480p.mp4',
+            'videos/sec1-2/sec1-2-720p.mp4',
+            'videos/sec3/sec3-720p.mp4',
+            'videos/sec6/sec6-480p.mp4',
+            'videos/sec7/sec7-480p.mp4',
+            'videos/sec8-9-10/sec8-9-10-480p.mp4',
         ]
     }
 }
